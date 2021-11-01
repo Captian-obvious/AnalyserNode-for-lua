a370e45aff8c7f690c8cb854e91ad6c244660bef
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-01 07:44:07 -0400

    Update analyserNode.lua

--- analyserNode.lua
+++ analyserNode.lua
@@ -24,24 +24,16 @@ function analyserNode.CreateAnalyser(s, source)
             end
         end
         function object:GetByteFrequencyData(inverseBool, src) --if not already specified this line allows an src to be changed--
-            local arrray = {}
+            local array = {}
             if (src~=nil and source == nil) then
                 source = src
             end
-            local fbc = object.frequencyBinCount
-            local pl = source.PlaybackLoudness
-            for i=1,fbc do
-                array[i] = complex.new(getSmoothWave((pl*i/fbc)), 0)--complex.new(math.random(), 0)
+            local p = source.PlaybackLoudness
+            local v = p * object.frequencyBinCount
+            local nv = getPV(p)
+            while i<#object.frequencyBinCount do
+                runServ.Stepped:Wait()
             end
-            local function divide(list, factor)
-                local newList = {}
-                for i,v in ipairs(list) do newList[i] = list[i] / factor end
-                return newList
-            end
-            local newArray = fft(array, inverseBool)
-            
-            local newArray1 = divide(newArray, fbc)
-            return newArray
         end
         return object
     end
