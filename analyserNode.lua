205e46b5bd7a9fd4154c302d23c10c3c1577e6a1
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-01 07:51:25 -0400

    Update analyserNode.lua

--- analyserNode.lua
+++ analyserNode.lua
@@ -4,6 +4,12 @@ function getSmoothWave(x)
     return math.sin(tick())*(x*2*math.pi)*255
 end
 
+--PV stands for Perfered Value
+
+function getPV(value, frequencyBinCount)
+    
+end
+
 function analyserNode.CreateAnalyser(s, source)
     if (s < 2^5 or s > 2^15) then
         error("DOM Exception: Index Size Error")
@@ -21,15 +27,17 @@ function analyserNode.CreateAnalyser(s, source)
                 object.fftSize = newSize
             end
         end
-        function object:GetByteFrequencyData(inverseBool, src) --if not already specified this line allows an src to be changed--
+        function object:GetByteFrequencyData(src, max) --if not already specified this line allows an src to be changed--
             local array = {}
             if (src~=nil and source == nil) then
                 source = src
             end
             local p = source.PlaybackLoudness
+            array[1] = p * 255
+            array[2] = p * 245
             local v = p * object.frequencyBinCount
-            local nv = getPV(p)
-            while i<#object.frequencyBinCount do
+            local nv = getPV(p, object.frequencyBinCount)
+            while i<#object.frequencyBinCount - 2 do
                 runServ.Stepped:Wait()
             end
         end
