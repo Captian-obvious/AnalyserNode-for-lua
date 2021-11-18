221b31ff26fc6d800e812383f39e1f01d5559d1d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-18 17:44:22 -0500

    Update analyserNode.lua

--- analyserNode.lua
+++ analyserNode.lua
@@ -1,22 +1,22 @@
 local analyserNode = {}
 
 function getSmoothWave(x)
-    return math.sin(tick())*(x*2*math.pi)*255
+	return math.sin(tick())*(x*2*math.pi)*255
 end
 
 --PV stands for Perfered Value
 
 function quadIn(t,b,c,d) 
-    t=t/d; 
-    return c*t*t+b; 
+	t=t/d; 
+	return c*t*t+b; 
 end;
 function quadOut(t,b,c,d) 
-    t=t/d; 
-    return -c*t*(t-2)+b; 
+	t=t/d; 
+	return -c*t*(t-2)+b; 
 end;
 
 function inverse(n)
-    return (1/n)
+	return (1/n)
 end
 
 function CheckSet(array,N,N1,S,D) -- Number, Scale, Direction
@@ -27,8 +27,8 @@ function CheckSet(array,N,N1,S,D) -- Number, Scale, Direction
 end
 
 function Set(array,fbc,N,S,D) -- Number, Scale, Direction
-	if N < #Bars - 2 then
-        array[N] = S
+	if N < fbc - 2 then
+		array[N] = S
 		if N>0 and D~=1 then -- Checks left for smaller bars to manipulate
 			CheckSet(array,N,N-1,S,-1)
 		end
@@ -41,40 +41,40 @@ function Set(array,fbc,N,S,D) -- Number, Scale, Direction
 end
 
 function analyserNode.CreateAnalyser(s, source)
-    if (s < 2^5 or s > 2^15) then
-        error("DOM Exception: Index Size Error")
-        return
-    else
-        local object = {
-            frequencyBinCount=s/2;
-            fftSize = s;
-        }
-        function object:SetFFTSize(newSize)
-            if (newSize < 2^5 or newSize > 2^15) then
-                error("DOM Exception: Index Size Error")
-                return
-            else
-                object.fftSize = newSize
-            end
-        end
-        function object:GetByteFrequencyData(src, max) --if not already specified this line allows an src to be changed--
-            local array = {}
-            if (src~=nil and source == nil) then
-                source = src
-            end
-            local i = 0
-            local p = src.PlaybackLoudness
-            array[1] = p * 255
-            array[2] = p * 245
-            local v = inverse(math.floor(p * object.frequencyBinCount))
-            local pv = p * 255
-            if (v * object.frequencyBinCount) > 2 then
-                Set(array,object.frequencyBinCount,object.frequencyBinCount*v,pv,D)
-            end
-            return array
-        end
-        return object
-    end
+	if (s < 2^5 or s > 2^15) then
+		error("DOM Exception: Index Size Error")
+		return
+	else
+		local object = {
+			frequencyBinCount=s/2;
+			fftSize = s;
+		}
+		function object:SetFFTSize(newSize)
+			if (newSize < 2^5 or newSize > 2^15) then
+				error("DOM Exception: Index Size Error")
+				return
+			else
+				object.fftSize = newSize
+			end
+		end
+		function object:GetByteFrequencyData(src, max) --if not already specified this line allows an src to be changed--
+			local array = {}
+			if (src~=nil and source == nil) then
+				source = src
+			end
+			local i = 0
+			local p = src.PlaybackLoudness
+			array[1] = p * 255
+			array[2] = p * 245
+			local v = inverse(math.floor(p * object.frequencyBinCount))
+			local pv = p * 255
+			if (v * object.frequencyBinCount) > 2 then
+				Set(array,object.frequencyBinCount,object.frequencyBinCount*v,pv,0)
+			end
+			return array
+		end
+		return object
+	end
 end
 
 return analyserNode
