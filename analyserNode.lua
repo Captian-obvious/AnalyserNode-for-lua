98bf568863e94523590d4c747c0e7eba728fc6ac
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-18 17:42:14 -0500

    Update analyserNode.lua

--- analyserNode.lua
+++ analyserNode.lua
@@ -19,6 +19,27 @@ function inverse(n)
     return (1/n)
 end
 
+function CheckSet(array,N,N1,S,D) -- Number, Scale, Direction
+	local nS=array[N]
+	if S>nS then
+		Set(array,N1,nS+(S-nS)/3,D)
+	end
+end
+
+function Set(array,fbc,N,S,D) -- Number, Scale, Direction
+	if N < #Bars - 2 then
+        array[N] = S
+		if N>0 and D~=1 then -- Checks left for smaller bars to manipulate
+			CheckSet(array,N,N-1,S,-1)
+		end
+		if N<fbc and D~=-1 then -- Checks right...
+			CheckSet(array,N,N+1,S,1)
+		end
+	else
+		return
+	end
+end
+
 function analyserNode.CreateAnalyser(s, source)
     if (s < 2^5 or s > 2^15) then
         error("DOM Exception: Index Size Error")
@@ -42,17 +63,18 @@ function analyserNode.CreateAnalyser(s, source)
                 source = src
             end
             local i = 0
-            local p = source.PlaybackLoudness
+            local p = src.PlaybackLoudness
             array[1] = p * 255
             array[2] = p * 245
-            i = 3
             local v = inverse(math.floor(p * object.frequencyBinCount))
             local pv = p * 255
-            if v > 2 then
-                array[object.frequencyBinCount*v] = pv
+            if (v * object.frequencyBinCount) > 2 then
+                Set(array,object.frequencyBinCount,object.frequencyBinCount*v,pv,D)
             end
             return array
         end
         return object
     end
 end
+
+return analyserNode
