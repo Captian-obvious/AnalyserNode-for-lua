37eaf82562f28476ac5225f9e99fde48b38762ab
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-21 15:04:37 -0500

    Update analyserNode.lua

--- analyserNode.lua
+++ analyserNode.lua
@@ -7,59 +7,59 @@ end
 --PV stands for Perfered Value
 
 function inverse(n)
-if n>0 then
-return (n^(-1))
-else
-warn("Cannot Divide By Zero")
-return 0
-end
+    if n>0 then
+        return (n^(-1))
+    else
+        warn("Cannot Divide By Zero")
+        return 0
+    end
 end
 
 function analyserNode.CreateAnalyser(s)
-if (s < 2^5 or s > 2^15) then
-error("DOM Exception: Index Size Error")
-return
-else
-local object = {
-frequencyBinCount=s/2;
-fftSize = s;
-};
-function object:SetFFTSize(newSize)
-if (newSize < 2^5 or newSize > 2^15) then
-error("DOM Exception: Index Size Error")
-return
-else
-object.fftSize = newSize
-end
-end
-function object:GetByteFrequencyData(audio) --if not already specified this line allows an src to be changed--
-local array = {}
-local pl = audio.PlaybackLoudness
-array[1] = pl * 255
-array[2] = pl * 245
-local v = inverse(math.floor(pl * object.frequencyBinCount))
-local pv = pl * 255
-local n = v*object.frequencyBinCount
-if n > 2 then
-array[n] = pv
-if n>0 then
-local nS=array[n-1]
-local n1 = n-1
-if pv>nS then
-array[n1] = nS+(pv-nS)/3
-end
-end
-if n<object.frequencyBinCount then
-local nS=array[n+1]
-local n1 = n+1
-if pv>nS then
-array[n1] = nS+(pv-nS)/3
-end
-end
-end
-return array
-end
-return object
-end
+    if (s < 2^5 or s > 2^15) then
+        error("DOM Exception: Index Size Error")
+        return
+    else
+        local object = {
+            frequencyBinCount=s/2;
+            fftSize = s;
+        };
+        function object:SetFFTSize(newSize)
+            if (newSize < 2^5 or newSize > 2^15) then
+                error("DOM Exception: Index Size Error")
+                return
+            else
+                object.fftSize = newSize
+            end
+        end
+        function object:GetByteFrequencyData(audio) --if not already specified this line allows an src to be changed--
+            local array = {}
+            local pl = audio.PlaybackLoudness
+            array[1] = pl * 255
+            array[2] = pl * 245
+            local v = inverse(math.floor(pl * object.frequencyBinCount))
+            local pv = pl * 255
+            local n = v*object.frequencyBinCount
+            if n > 2 then
+                array[n] = pv
+                if n>0 then
+                    local nS=array[n-1]
+                    local n1 = n-1
+                    if pv>nS then
+                        array[n1] = nS+(pv-nS)/3
+                    end
+                end
+                if n<object.frequencyBinCount then
+                    local nS=array[n+1]
+                    local n1 = n+1
+                    if pv>nS then
+                        array[n1] = nS+(pv-nS)/3
+                    end
+                end
+            end
+            return array
+        end
+        return object
+    end
 end
 return analyserNode
