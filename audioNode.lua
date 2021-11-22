54e687f4c758eeb43c17acce5b240aa3e5585cfa
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-22 11:13:58 -0500

    Update audioNode.lua

--- audioNode.lua
+++ audioNode.lua
@@ -1,4 +1,15 @@
-local analyserNode = require(script.Parent.AnalyserNode)
+local analyserNode = require(script.Parent.AnalyserNode);
 local audioNode = {};
 
-return audioNode
+function audioNode.new()
+    local object = {};
+    function object.CreateAnalyser(fftSize)
+        local object = analyserNode.CreateAnalyser(fftSize)
+        return object
+    end
+    function object.CreateGain()
+        
+    end
+end
+
+return audioNode;
