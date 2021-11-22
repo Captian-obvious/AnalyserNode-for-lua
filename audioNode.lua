be71325853c38aeced79aeb4ca8462f32cccbbdb
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-22 11:28:43 -0500

    Update audioNode.lua

--- audioNode.lua
+++ audioNode.lua
@@ -1,17 +1,22 @@
 local analyserNode = require(script.AnalyserNode);
+
 local audioNode = {};
 
 function audioNode.new(src)
     local object = {};
     if not src then
-        src = Instance.new("Sound",workspace)
-    end
-    object.Source = src
+        src = Instance.new("Sound",workspace);
+    end;
+    object.Source = src;
     function object.CreateAnalyser(fftSize)
-        return analyserNode.CreateAnalyser(src, fftSize)
-    end
+        return analyserNode.CreateAnalyser(src, fftSize);
+    end;
     function object.CreateGain()
-        
+        local object = {};
+        function object.SetGainAtTime(multiplier);
+            local max = 10
+            src.Volume = max * multiplier
+        end
     end
 end
 
